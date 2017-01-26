a69f3e6f867f6e648a32ae82668b0e20ffe27ec3
WilliamSavage <williamsavage@hotmail.com>
2017-01-26 15:21:21 +0000

    Js for marble in box responding to orientation of device

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
+var box = document.getElementById('marbleBox');
+
 var marble = document.getElementById('marble');
 
 var hole = document.getElementById('hole');
+
+if(hole.style.top) {
+  
+}
+
 hole.addEventListener('click', () => {
-  marble.style.top = '30%'
-  marble.style.left = '30%'
+  marble.style.top = '70%'
+  marble.style.left = '70%'
 })
+
+var maxX = box.clientWidth  - marble.clientWidth;
+var maxY = box.clientHeight - marble.clientHeight;
 
 window.addEventListener("deviceorientation", handleOrientation, true);
 
 function handleOrientation(event) {
   var absolute = event.absolute;
-  var alpha    = event.alpha;
-  var beta     = event.beta;
-  var gamma    = event.gamma;
+  var z    = event.alpha;
+  var x     = (event.beta + 90)/1.8;
+  var y    = (event.gamma + 90)/1.8;
 
-  marble.style.top = '10%'
-  marble.style.left = '10%'
-  // Do stuff with the new orientation data
+  if(y===90) y
+  marble.style.top  = x + '%';
+  marble.style.left = y + '%';
+
+  const output = document.getElementsByClassName('output');
+  output.innerHTML  = "beta : " + x + "\n";
+  output.innerHTML += "gamma: " + y + "\n";
 }
