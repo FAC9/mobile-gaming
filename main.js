ff9a3cb5ccc016d96c80737205b08b8db31bcd5d
WilliamSavage <williamsavage@hotmail.com>
2017-01-26 14:11:38 +0000

    initalise html css and js files with start

---
+++ main.js
@@ -0,0 +1,8 @@
+var marble = document.getElementById('marble');
+
+
+var hol = document.getElementById('hole');
+hol.addEventListener('click', () => {
+  marble.style.top = '30%'
+  marble.style.left = '30%'
+})
